02695c2217439b11867c9f0f121b041acbf3c9ad
Patrick Celentano <patrick_celentano@yahoo.com>
2023-11-23 10:11:25 -0600

    More cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pages-test/",
+  base: "/pages-test/", // GitHub pages
+  server: { port: 3000 },
   plugins: [react()],
 })
