7abccf41007e0a3138ac2c91cebe634cb5897bfd
Patrick Celentano <patrick_celentano@yahoo.com>
2023-11-23 11:12:10 -0600

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pages-test/', // GitHub pages
+  base: "/pages-test/",
   plugins: [react()],
 })
